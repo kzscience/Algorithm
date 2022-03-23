ea6f26ecb8f746159c737198f1ec574cce8e3b50
QypshaQ <QypshaQ777@gmail.com>
2022-03-24 01:22:46 +0600

    Update quick_sort.cpp

--- C++/17/Grokking Algorithms/Chapter_3/quick_sort.cpp
+++ C++/17/Grokking Algorithms/Chapter_3/quick_sort.cpp
@@ -7,28 +7,47 @@ const int ZERO = []() {
 	std::cout.tie(nullptr);
 	return 0;
 }();
-
 template <typename T>
-T sum(std::vector<T> arr) {
-	if (arr.empty()) {
-		return 0;
-	}
+std::vector<T> quicksort(const std::vector<T>& arr) {
 
-    T last_num = arr.back(); 
-    arr.pop_back(); 
-    return last_num + sum(arr);
+    if (arr.size() < 2)
+        return arr;
+
+    const T* pivot = &arr.front() + arr.size() / 2 - 1; 
+    std::vector<T> less;  
+    std::vector<T> greater;  
+
+    for (const T* item = &arr.front(); item <= &arr.back(); item++) {
+        if (item == pivot) {
+            continue;
+        }
+        if (*item <= *pivot) {
+            less.push_back(*item);
+        }
+        else greater.push_back(*item);
+    }
+
+    std::vector<T> sorted_less = quicksort(less);
+    std::vector<T> sorted_greater = quicksort(greater);
+    sorted_less.push_back(*pivot);
+    sorted_less.insert(sorted_less.end(), sorted_greater.begin(), sorted_greater.end());
+
+    return sorted_less;
 }
 
 int main() {
-	int size, x;
+    int size, element;
+    std::cin >> size;
     std::vector<int> arr;
-	std::cin >> size;
+    for (int i = 0; i < size; i++) {
+        std::cin >> element;
+        arr.push_back(element);
+    }
 
-	for (int  i = 0; i < size; i++)
-	{
-		cin >> x;
-		arr.push_back(x);
-	}
 
-    cout << "Sum ints: " << sum(arr) << endl;
+    std::vector<int> sorted = quicksort(arr);
+    for (int num : sorted) {
+        cout << num << " ";
+    }
+    cout << endl;
 }
